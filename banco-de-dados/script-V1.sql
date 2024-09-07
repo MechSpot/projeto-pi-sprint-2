82db7eb56ee01398d4d60aa34bb2963f4cccbafd
NicolasNun <nicolas.santos010@sptech.school>
2024-09-07 16:36:29 -0300

    Atualização banco de dados

--- banco-de-dados/script-V1.sql
+++ banco-de-dados/script-V1.sql
@@ -16,16 +16,15 @@ telefone char(11)
 
 create table boxe (
 id_boxe int primary key auto_increment,
-local_boxe int,
-foreign key (local_boxe) references oficina_concessonaria(id_oficina)
+local_boxe int not null
 );
 
 create table usuario (
 id_usuario int primary key auto_increment,
-fk_oficina int,
+oficina int not null,
+nome varchar(45) not null,
 email varchar(50) not null,
-senha varchar(20) not null,
-foreign key (fk_oficina) references oficina_concessonaria(id_oficina)
+senha varchar(20) not null
 );
 
 create table sensor (
@@ -33,33 +32,31 @@ id_sensor int primary key auto_increment,
 status_sensor varchar(9) not null,
 constraint chk_sensor check (status_sensor in('operante', 'inoperante')),
 local_sensor int,
-foreign key (local_sensor) references boxe(id_boxe),
 instalacao_boxe int
 );
 
 create table registro (
 id_registro int primary key auto_increment,
-fk_sensor int,
-resultado int,
-dt_hora datetime,
-foreign key (fk_sensor) references sensor(id_sensor)
+sensor_origem int not null,
+resultado int not null,
+dt_hora datetime not null
 );
 
 -- inserção de valores nas tabelas
 insert into oficina_concessonaria (nome_fantasia, cnpj, email, cep, num_endereco, celular, total_boxes, telefone) values
-('Oficina ABC', '12.345.678/0001-90', 'contato@oficinaabc.com', '12345-678', 100, '99 999999999', 7, '31 34455666');
+('Hyundai', '12.345.678/0001-90', 'hyundai@gmail.com', '12345-678', 100, '99 999999999', 10, '31 34455666');
 
 insert into boxe (local_boxe) values
 (1);
 
-insert into usuario (fk_oficina, email, senha) values
-(1, 'usuario@oficinaabc.com', 'senhaSegura123');
+insert into usuario (oficina, nome, email, senha) values
+(1, 'Peter Quill', 'peter.quill@gmail.com', 'senhaSegura123');
 
-insert into sensor (status_sensor, local_sensor) values
-('operante', 1);
+insert into sensor (status_sensor, local_sensor, instalacao_boxe) values
+('operante', 1, 1);
 
-insert into registro (fk_sensor, resultado, dt_hora) values
-(1, 1, '2024-09-03 14:30:00');
+insert into registro (sensor_origem, resultado, dt_hora) values
+(1, 1, '2024-09-07 16:34:05');
 
 -- visualização dos registros
-select fk_sensor as sensor, resultado, dt_hora as 'Data e Hora' from registro;
+select sensor_origem as sensor, resultado, dt_hora as 'Data e Hora' from registro;
