0521fe99b56d0e17346a1247947c5ead2de7cf25
NicolasNun <nicolas.santos010@sptech.school>
2024-10-12 13:34:13 -0300

    Organizando os diretórios

--- public/js/login_cadastro.js
+++ public/js/login_cadastro.js
@@ -12,9 +12,9 @@ function entrar() {
   const email = input_email_login.value;
   const senha = input_senha_login.value;
 
-  if (email == 'Honda' && senha == '12345678') {
-    window.location = "./dashboard.html"
+  if (email == "Honda" && senha == "12345678") {
+    window.location = "./visao_geral.html";
   } else {
-    alert("Dados invalidos")
+    alert("Dados invalidos");
   }
 }
