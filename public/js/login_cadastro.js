619f688a531b8ea3af87bd720183defd806d3ca2
NicolasNun <nicolas.santos010@sptech.school>
2024-10-19 23:51:26 -0300

    Subindo validações de login e cadastro

--- public/js/login_cadastro.js
+++ public/js/login_cadastro.js
@@ -12,9 +12,37 @@ function entrar() {
   const email = input_email_login.value;
   const senha = input_senha_login.value;
 
-  if (email == "Honda" && senha == "12345678") {
+  if (email == "honda@gmail.com" && senha == "Honda123") {
     window.location = "./visao_geral.html";
   } else {
     alert("Dados invalidos");
+  }
+}
+
+function cadastrar() {
+  var nome = input_nome.value;
+  var email = input_email.value;
+  var senha = input_senha.value;
+
+  var senhaPossuiMaiscula = false;
+  var senhaPossuiMinuscula = false;
+  var senhaPossuiCaracterEspecil = false;
+
+  if (senha[caracterAtual] == senha[caracterAtual].toUpperCase()) {
+      senhaPossuiMinuscula == true;
+  } else if (senha[caracterAtual] == senha[caracterAtual].toLowerCase()) {
+      senhaPossuiMaiscula == true;
+  }
+
+  if (nome.length < 3) {
+    alert("Nome muito curto");
+  } else if (email.indexOf("@") == -1 || !email.endsWith(".com")) {
+    alert("Email Inválido");
+  } else if (
+    senha.length < 8 ||
+    !senhaPossuiMaiscula ||
+    !senhaPossuiMinuscula
+  ) {
+    alert("Senha Inválida");
   }
 }
