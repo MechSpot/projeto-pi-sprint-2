1792a1251562e31beacb5c9b2931574156985669
NicolasNun <nicolas.santos010@sptech.school>
2024-10-28 21:24:53 -0300

    Validações do cadastro da empresa

--- public/js/login_cadastro.js
+++ public/js/login_cadastro.js
@@ -20,41 +20,70 @@ function entrar() {
 }
 
 function cadastrar() {
-  var nome = input_nome.value;
-  var email = input_email.value;
-  var senha = input_senha.value;
+  const cnpj = input_cnpj.value;
+  const razaoSocial = input_razao_social.value;
+  const representanteLegal = input_representante_legal.value;
+  const email = input_email.value;
+  const telefoneCelualar = input_tel_celular.value;
+  const telefoneFixo = input_tel_fixo.value;
+  const senha = input_senha.value;
 
-  var senhaPossuiMaiscula = false;
-  var senhaPossuiMinuscula = false;
-  var senhaPossuiNumero = false;
+  const camposPreenchidos =
+                  cnpj != "" &&
+           razaoSocial != "" &&
+    representanteLegal != "" &&
+                 email != "" &&
+      telefoneCelualar != "" &&
+                 senha != "";
+  if (!camposPreenchidos) {
+    alert("Preencha todos os campos para continuar");
+  } else {
+    var senhaPossuiMaiscula = false;
+    var senhaPossuiMinuscula = false;
+    var senhaPossuiNumero = false;
 
-  for (var caracterAtual = 0; caracterAtual < senha.legth; caracterAtual++) {
-    if (!senhaPossuiMaiscula || !senhaPossuiMinuscula) {
-      if (senha[caracterAtual] == senha[caracterAtual].toUpperCase()) {
-        senhaPossuiMaiscula = true;
-      } else if (senha[caracterAtual] == senha[caracterAtual].toLowerCase()) {
-        senhaPossuiMinuscula = true;
+    for (var caracterAtual = 0; caracterAtual < senha.length; caracterAtual++) {
+      if (!senhaPossuiMaiscula || !senhaPossuiMinuscula) {
+        if (senha[caracterAtual] == senha[caracterAtual].toUpperCase()) {
+          senhaPossuiMaiscula = true;
+        } else if (senha[caracterAtual] == senha[caracterAtual].toLowerCase()) {
+          senhaPossuiMinuscula = true;
+        }
+      }
+
+      if (!senhaPossuiNumero) {
+        if (!isNaN(Number(senha[caracterAtual]))) {
+          senhaPossuiNumero = true;
+        }
       }
     }
 
-    if (!senhaPossuiNumero) {
-      if (!isNaN(Number(senha[caracterAtual]))) {
-        senhaPossuiNumero = true;
-      }
+    if (cnpj.length < 14) {
+      alert("Cnpj inválido");
+    } else if (razaoSocial.length < 5 || razaoSocial.length > 100) {
+      alert("Razão social Inválida");
+    } else if (
+      representanteLegal.length < 5 ||
+      representanteLegal.length > 100
+    ) {
+      alert("Represemtante legal Inválido");
+    } else if (email.indexOf("@") == -1 || !email.endsWith(".com")) {
+      alert("Email Inválido");
+    } else if (telefoneCelualar.length < 11) {
+      alert("Número de celular inválido");
+    } else if (telefoneFixo.legth < 10) {
+      alert("Número de telefone fixo inválido");
+    } else if (
+      senha.length < 8 ||
+      !senhaPossuiMaiscula ||
+      !senhaPossuiMinuscula ||
+      !senhaPossuiNumero ||
+      senha.indexOf(" ") != -1
+    ) {
+      alert("Senha Inválida");
+    } else {
+      alert("Login Efetuado Com Sucesso!!")
+      trocar_tela()
     }
-  }
-
-  if (nome.length < 3) {
-    alert("Nome muito curto");
-  } else if (email.indexOf("@") == -1 || !email.endsWith(".com")) {
-    alert("Email Inválido");
-  } else if (
-    senha.length < 8 ||
-    !senhaPossuiMaiscula ||
-    !senhaPossuiMinuscula ||
-    !senhaPossuiNumero ||
-    senha.indexOf(" ") != -1
-  ) {
-    alert("Senha Inválida");
   }
 }
