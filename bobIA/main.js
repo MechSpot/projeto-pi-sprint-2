85e211d62acfa8b6647b1ddf74002bda1c6cb4a6
Gusta-PA <gustavo.almeida@sptech.school>
2024-12-04 16:52:37 -0300

    BobIA

---
+++ bobIA/main.js
@@ -0,0 +1,75 @@
+// importando os bibliotecas necessárias
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
+const path = require("path");
+
+// carregando as variáveis de ambiente do projeto do arquivo .env
+require("dotenv").config();
+
+// configurando o servidor express
+const app = express();
+const PORTA_SERVIDOR = process.env.PORTA;
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
+
+// configurando o servidor para receber requisições JSON
+app.use(express.json());
+
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// configurando CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+// inicializando o servidor
+app.listen(PORTA_SERVIDOR, () => {
+    console.info(
+        `
+        ######                ###    #    
+        #     #  ####  #####   #    # #   
+        #     # #    # #    #  #   #   #  
+        ######  #    # #####   #  #     # 
+        #     # #    # #    #  #  ####### 
+        #     # #    # #    #  #  #     # 
+        ######   ####  #####  ### #     # 
+        `
+    );
+    console.info(`A API BobIA iniciada, acesse http://localhost:${PORTA_SERVIDOR}`);
+});
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json( { resultado } );
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+    // obtendo o modelo de IA
+    const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const resultado = await modeloIA.generateContent(`Em um paragráfo responda: ${mensagem}`);
+        const resposta = await resultado.response.text();
+        
+        console.log(resposta);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
